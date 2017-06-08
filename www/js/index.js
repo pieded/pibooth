9e03163eb54803f9d87f2bcac1f65ebd7adbb647
pieded <pierrededy@gmail.com>
2017-06-08 22:16:10 +0200

    add preview canvas as overlay

--- www/js/index.js
+++ www/js/index.js
@@ -9,17 +9,35 @@
 
 const noop = () => {};
 
+const snapWidth = 1920;
+const snapHeight = 1080;
+
 const mediaConstraints = {
     video: {
-        width: 1920,
-        height: 1080
+        width: snapWidth,
+        height: snapHeight
     }
 };
+
+const canvas = document.getElementById('preview');
+canvas.width = snapWidth;
+canvas.height = snapHeight;
+const preview = canvas.getContext('2d');
+const video = document.getElementById('video');
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia(mediaConstraints)
         .then(function(localMediaStream) {
-            const video = document.querySelector('video');
             video.src = window.URL.createObjectURL(localMediaStream);
         }).catch(noop);
 }
+
+// Trigger photo take
+document.getElementById('snap').addEventListener('click', function() {
+    preview.drawImage(video, 0, 0, snapWidth, snapHeight);
+    canvas.style.opacity = 1;
+    setTimeout(() => {
+        canvas.style.opacity = 0;
+    },
+    1000);
+});
