13cc94116a130edb3f754dfd51e9b20fdd064e24
pieded <pierrededy@gmail.com>
2017-06-26 21:44:34 +0200

    search for port and use opn programmatically

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const opn = require('opn');
+const portfinder = require('portfinder');
 
+const certDir = path.join(__dirname, 'cert');
 const docroot = path.join(__dirname, 'www');
 const snaps = path.join(__dirname, 'snaps');
 const filenameFormat = 'YYYY-MM-DD_HH:mm:ss:SS';
-const serverPort = 8080;
+
+const raspiconf = {app: ['chromium-browser', '--noerrdialogs', '--kiosk']};
+const opnConfig = {} || raspiconf;
 
 const app = express();
+
+const privateKey = fs.readFileSync(path.join(certDir, 'localhost.key'), 'utf8');
+const certificate = fs.readFileSync(path.join(certDir, 'localhost.crt'), 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
 
 app.use(express.static(docroot));
 
@@ -47,6 +58,37 @@ app.use((err, req, res, next) => {
     next(err);
 });
 
-app.listen(serverPort, () => {
-    console.log('server started on port', serverPort);
-});
+const httpsServer = https.createServer(credentials, app);
+
+const gracefulShutdown = function () {
+    console.log('Received kill signal, shutting down gracefully.');
+    httpsServer.close(function () {
+        console.log('Closed out remaining connections.');
+        process.exit();
+    });
+
+    // if after
+    setTimeout(function () {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit();
+    }, 10 * 1000);
+};
+
+process.on('SIGTERM', gracefulShutdown);
+
+process.on('SIGINT', gracefulShutdown);
+
+const listenToPort = (port) => {
+    httpsServer.listen(port, () => {
+        console.log('server started on port', port);
+        opn('https://localhost' + ':' + port, opnConfig);
+    });
+};
+
+portfinder.getPortPromise()
+    .then((port) => {
+        listenToPort(port);
+    })
+    .catch((err) => {
+
+    });
